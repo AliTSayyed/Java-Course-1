e235b259ef864dd64b1693c31f42953a48ff17a6
ATS <ATS@Ali-Taabeshs-MacBook-Pro.local>
2023-09-21 12:26:52 -0500

    initial commit

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,7 @@
+
+public class Sandbox { //start of our class
+
+    public static void main(String[] args) { // start of our main
+       System.out.println("First Java Line of Code");
+    } // end of our main
+} //  end of our class
