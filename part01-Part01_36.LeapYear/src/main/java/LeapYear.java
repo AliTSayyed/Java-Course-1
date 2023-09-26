a9740e692807cdec85436c3cbf0fd04c7bc8e484
ATS <ATS@Ali-Taabeshs-MacBook-Pro.local>
2023-09-26 11:35:07 -0500

    part 2 of course materials

--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -5,6 +5,7 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+       
         System.out.println("Give a year: ");
         int year = Integer.valueOf(scan.nextLine());
         if (year % 4 != 0) {
