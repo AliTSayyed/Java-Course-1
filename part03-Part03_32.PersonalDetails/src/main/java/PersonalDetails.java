2438968a79c75931169b19c599aabea40988306e
ATS <ATS@Ali-Taabeshs-MacBook-Pro.local>
2023-10-09 13:23:04 -0500

    parts 3_28 to 3_32 completed

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -5,8 +5,37 @@ import java.util.Scanner;
 public class PersonalDetails {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-
+        Scanner scanner = new Scanner(System.in);              
+        
+        int sum = 0;        
+        int count = 0;
+        int length = 0;
+        String name = "name";
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String pieces[] = input.split(",");
+            int year = Integer.valueOf(pieces[1]);
+            sum = sum + year;
+            count++;
+            
+            if (input.length() > length){
+                length = input.length();
+                name = pieces[0];
+            }                
+        }
+        
+        double avg = 1.0 * sum / count;
+        
+        System.out.println("Longest name: " + name);
+        
+        if (count == 0){
+            System.out.println("No input given");
+        } else {
+            System.out.println("Average of the birth years: " + avg);
+        }
     }
 }
